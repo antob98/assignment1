cf037606788002eeea25f0b69883643572670989
antob98 <s5194849@studenti.unige.it>
2021-11-09 10:32:47 +0000

    assignment_inclusion

---
+++ README_ASS.md
@@ -0,0 +1,72 @@
+
+#**Conents of the Assingment_1 Repository**:
+
+
+The contents of this repository delve into my own solution for the 1st graded assignment given to us in the Research Track class of the Robotics Engineering Master course held in Università degli Studi di Genova. The simulator I used has been provided to us by our professor, so if you are interested in the explaination of all its functionalities or want to know more about the built-in Robot API you can follow the specifications listed in [CarmineD8's Github Repository](https://github.com/CarmineD8/python_simulator), specifically in the branch "assignment".
+
+##**Objectives and Use-Instructions:**
+
+The code produced for this assignment is contained in the file *assignment_1.py* and has been carried out in python2. Other than that, we produced this README to explain its contents.  
+The assignment was:  
+> Write a python script for achieving this robot’s behaviour:
+> Constrantly drive the robot around the circuit in the counter-clockwise direction and
+> avoid touching the golden boxes. When the robot is close to a silver box, it should 
+> grab it, and move it behind itself.  
+The code we produced handles all points of the assignment without any problems. There is to note, though, that because of the way we wrote it there is an extremely niche case in which the robot could "turn" to avoid a specific obstacle, inverting its trajectory in the given circuit. The specific case may happen when the robot goes straight into an angle at a perfect 45° angle, being exactly at the same distance from both lateral walls. This uncertainty of its course of actions derives from the fact that in this case it is impossible for the robot to determine where it has to go to remain in counter-clockwise motion without it knowing at every instant its current position and orientation with respect tp the specific circuit. This can be avoided using a different approach, described in the "*Possible Improvements*" section.
+
+> To run our code, get in the correct folder from the shell and write the command: `python2 run.py assignment_1.py`.
+
+###**Code Behaviour (flowchart):**
+
+The robot keeps looping 8 separate checks, each leading to their own instructions. 
+While looping:
+- If a golden token is in the frontal cone, take its position and orientation. 
+- If a silver token is in the frontal cone, take its position and orientation.
+- If a golden token is in the left cone, take its position and orientation. 
+- If a golden token is in the right cone, take its position and orientation.
+- If a golden token the distance of the golden token in front is less than set_dist:
+   If there was a golden token in the left cone and its distance was less than 2: 
+    If there also was a golden token in the right cone and its distance was less than 2: 
+     If the distance of the right token was greater than the distance of the left token:
+      Turn right.
+     Else if the distance of the left token was greater than the distance of the right token: 
+      Turn left.
+    Else:
+     Turn right.
+   Else if there was a golden token in the right cone and its distance was less than 2: 
+    Turn left.
+   Else:
+    If the orientation angle of the frontal token is lower than -a_th:
+     Turn right.
+    Else if the orientation angle of the frontal token is greater than a_th:
+     Turn left. 
+    Else if the orientation angle of the frontal token is greater than or equal to -a_th and strictly lower than 0:
+     Turn right.
+    Else:
+     Turn left.
+- Else if no silver token has been detected:
+   Drive forward.
+- Else if there was a silver token and its distance was less than d_th:
+   If you can grab it:
+    Turn around, release it, then turn around again.
+   Else: 
+    print "can't grab it yet" 
+- Else:
+   If the orientation angle of the token is lower than -a_th:
+    Turn left.
+   If the orientation angle of the token is greater than a_th:
+    Turn right.
+   Else:
+    Drive forward.
+
+####**Possible Improvements:**
+
+As we previously stated, there is one uncertainty in the robot's behaviour that could be improved upon. It is due to the nature of our coding: since we were specifically asked to build it with functions and types of instructions seen during the course, we didn't use all the characteristics of the robot written in the file containing its class definition. For this reason, even if the class robot provides the possibility to know the robot-instance's position and orientation at each time instant, we chose not to use them in our code. However, this creates a drawback: if the robot goes straight into a corner at a 45° angle, thus perceiving the same distance from both lateral walls, it will give priority to left-turning. If we are lucky, it will be the right choice to preserve counter-clockwise motion, but we cannot really know in advance when this will happen. When it does, the robot may change its moving direction and start moving clockwise (because maybe in that situation to keep moving counter-clockwise in the circuit it would've needed to go right). However, this isn't a critical issue (since the robot will keep avoiding golden tokens and grab silver ones without problems) and as far as we tested, given the configuration of this particular circuit this inconvenience may very well never happen (it would be almost impossible for the robot to get in that perfect orientation), so we left the code as is.  
+To solve this issue we recommend either:
+1. making the robot loosely know the intended path (code it in a way that allows it to have a mental map of which direction should he choose if it finds itself in this predicament at any time, and force its choice.), even if this would require drastic changes in the original code and many more lines of code (to the point where it may not be worth it). Using the robot's built-in class functionalities to take account of its position and orientation at each time instant may greatly lower the ammount of additional code, so you should do it. An easy way to solve it in this way would be fixing the starting position and  angle of the robot as standards for comparison, then checking (each time it finds itself in an angle) the relative position and orientation with respect to the standards: in this way you can know which direction should you choose to go based on your how the angle is placed and where you are in the circuit.
+2. making the robot know where it came from in terms of direction and position (maybe by memorizing a certain number of previous positions in time at runtime) and base its choice on that (to avoid taking the same path twice, thus also diverting from its intended motion).  
+However, this would tie the robot's correct behaviour to this particular circuit, limiting its potential for reuse. There is, in fact, no *"correct"* general choice for always going counter-clockwise in all possible circuits that present angles in their perimeter.
+
+
+
+
---
+++ robot-sim/assignment_1.py
@@ -0,0 +1,165 @@
+from __future__ import print_function
+import time
+from sr.robot import *
+
+"""
+This is my solution for the first Research Track Assignment of the Robotics Engineering Master Course held at University of Genova in the year 2021/2022. To run this code, get in the correct directory from the shell and use the command:
+	$ python2 run.py assignment_1.py
+"""
+
+'''Threshold for orientation control'''
+a_th=2.0
+'''Threshold for linear distance control'''
+d_th=0.4
+
+R = Robot()
+""" instance of the class Robot"""
+
+def drive(speed, seconds): #base function, provided by the standard robot documentation. We applied no changes to it.
+    """
+    Function for setting a linear velocity
+
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed #since this is a ... robot, applying the same speed to both motors will result in the robot moving forward at that speed for "seconds" seconds.
+    R.motors[0].m1.power = speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+def turn(speed, seconds): #base function, provided by the standard robot documentation. We applied no changes to it.
+    """
+    Function for setting an angular velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed #applying opposite velocities to the two motors will make the robot turn in place for "seconds" seconds.
+    R.motors[0].m1.power = -speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+#here goes the code
+'''this function allows the robot to find silver tokens present in front of it. 
+
+Its angular range of vision is 360 normally, and you can manually set the distance at which he's able to recognize objects. We reduced its vision capabilities to a cone of "dist" lenght and angle limited between the desired ext1 and ext2 set. We did this so that we could avoid.
+
+Whenever the robot sees silver tokens in its cone of vision, this function calculates which is the nearest one and returns its distance and orientation with respect to the robot. Otherwise, in case no silver tokens are detected in the cone, it returns -1.'''
+def find_silver_token(ext1,ext2):
+    dist=2
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER and ext1 <= token.rot_y <= ext2:
+            dist=token.dist
+	    rot_y=token.rot_y
+    if dist==2:
+        return -1, -1
+    else:
+        return dist, rot_y
+
+
+'''this function has the same structure as find_silver_token, but allows the robot to find the golden tokens that make up the perimeter of the circuit instead. 
+
+This time, the distance is higher because we want to keep processing all possible golden tokens in the cone to always know where the nearest one is. The distance threshold that decides when the robot will react is, in fact not defined here, but later in the main loop of our code. 
+
+This combination allows the robot to always know where is the nearest golden token in its surroundings, but also to act only when specifically needed.'''
+def find_wall(ext1,ext2):
+    dist=100
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_GOLD and ext1 <= token.rot_y <= ext2:
+            dist=token.dist
+	    rot_y=token.rot_y
+    if dist==100:
+        return -1, -1
+    else:
+        return dist, rot_y
+
+'''This function allows the robot to correctly orientate itself with respect to an object placed in the x,y position. We will use it to get near the silver tokens in the correct ofientation to allow the robot to grab them.'''       
+def rot_funct(x,y):
+    if y<-a_th: 
+        print("turn left")
+        turn(-2,0.5)
+    if y>a_th:
+        print("turn right")
+        turn(+2,0.5)
+    else:
+        print("moving!")
+        drive(50,0.1)
+
+
+'''Function that allows the robot to grab the token and leave it behind itself when it's ready to do so. The threshold for distance control that allows the class function "grab" to work is used in the main loop of our code.'''
+def grab_if(b,c,d):
+    print("found it")
+    if R.grab():
+        print("grabbed it")
+        turn(30,2)
+        R.release()
+        turn(-30,2)
+    else:
+        print("can't grab it yet")
+
+
+'''Function used to avoid obstacles (golden markers) and choose how to move based on the actual position and orientation of the robot with respect to the surrounding golden blocks.
+
+- When the robot has near walls on both sides (is at an angle of the perimeter) it will choose to move left or right based on the nearest perceived distance from the opposite side. - If, instead, it has only obstacles on the left or on the right it will move in the opposite direction. 
+- If the obstacle is perceived just in front, it will try to avoid it based on its relative orientation, trying to keep the same motion direction (counter-clockwise). 
+
+However, there is a case in which it may not be able to keep the same direction: if the nearest perceived golden block is directly in its front, with 0 as relative orientation and the same perceived distance from the lateral walls, then it will always prioritize left movement (because there's no way to tell which would be a correct direction).'''
+def move_funct(b,c,d):
+    print("iceberg detected")
+    if dest_sx!=-1 and dest_sx<2:
+        if dest_dx!=-1 and dest_dx<2:
+            print("surrounded!")
+            if dest_dx>dest_sx:
+                print("turn right!")
+                turn(+15,1)
+            elif dest_sx>=dest_dx:
+                print("turn left!")
+                turn(-15,1)
+        else:
+            print("forward+left: turn right!")
+            turn(+15,1)
+    
+    elif dest_dx!=-1 and dest_dx<2:
+        print("forward+right: turn left!")
+        turn(-15,1)
+    
+    else:
+        print("just forward!")
+        if d<-a_th:
+            print("avoiding collision: turn right")
+            turn(+15,1)
+        elif d>a_th:
+            print("avoiding collision: turn left")
+            turn(-15,1)
+        elif -a_th<=d<0:
+            print("iceberg in front! turn right!")
+            turn(+15,1)
+        else:
+            print("iceberg in front! turn left!")
+            turn(-15,1)
+
+
+set_dist=0.8 #threshold for distance control for golden tokens
+while 1:
+    #the angles used to shape the frontal and lateral cones of vision of the robot were the result of multiple tests. These ones gave the best performance and reliability overall.
+    c,d=find_wall(-40,40)
+    a,b=find_silver_token(-30,30)
+    dest_sx,rot_sx=find_wall(-105,-80)
+    dest_dx,rot_dx=find_wall(80,105)
+    
+    #priorities and behaviour
+    if c<set_dist: #priority is given to safety: 
+    #the robot should always avoid obstacles first
+        move_funct(b,c,d)
+    elif a==-1:
+        print("I can't see any silver token")
+        drive(50,0.1)     
+    elif a<d_th:
+        grab_if(b,c,d)
+    else:
+        print("moving!")
+        rot_funct(a,b)
+
+
